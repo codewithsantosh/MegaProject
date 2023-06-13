330e22f4ad594432b6b972db6e27aaa4a834ff7c
codewithsantosh <santoshpowar1997@gmail.com>
2023-06-13 12:40:09 +0530

    initial comment

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+from flask import Flask
+
+app = Flask(__name__)
+app.debug = True
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'Hello, {name}!'
+    return render_template('index.html')
